d335fe9d10b68fe1ef700ea18d7ff2a81f8e124e
efzedel16 <efzedel16@outlook.com>
2021-06-11 16:25:07 +0700

    Create UpdateUserById

--- backend/services/userService.go
+++ backend/services/userService.go
@@ -4,15 +4,16 @@ import (
 	"book_list/entities"
 	"book_list/inputs"
 	"book_list/repositories"
+	"errors"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService interface {
 	SignUpUser(input inputs.UserInputSignUp) (entities.User, error)
 	SignInUser(input inputs.UserInputSignIn) (entities.User, error)
-	UpdateUserById(id inputs.InputUserId, input inputs.UpdateUserInput) (entities.User, error)
+	UpdateUserById(inputId inputs.InputUserId, input inputs.UpdateUserInput) (entities.User, error)
 	// GetAllUsers() ([]formatters.UserDataFormatter, error)
-	GetUserById(id inputs.InputUserId) (entities.User, error)
+	GetUserById(inputId inputs.InputUserId) (entities.User, error)
 }
 
 type userService struct {
@@ -44,17 +45,40 @@ func (s *userService) SignUpUser(input inputs.UserInputSignUp) (entities.User, e
 }
 
 func (s *userService) SignInUser(input inputs.UserInputSignIn) (entities.User, error) {
-	user := entities.User{
-		Email:    input.Email,
-		Password: input.Password,
-	}
-
-	user, err := s.userRepository.FindUserByEmail(user.Email)
+	userEmail := input.Email
+	userPassword := input.Password
+	user, err := s.userRepository.FindUserByEmail(userEmail)
 	if err != nil {
 		return user, err
 	}
 
 	if user.Id == 0 {
-		return user
+		return user, errors.New("no user found with this email")
 	}
+
+	if err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userPassword)); err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (s *userService) UpdateUserById(inputId inputs.InputUserId, input inputs.UpdateUserInput) (entities.User, error) {
+	user, err := s.userRepository.FindUserById(inputId.Id)
+	if err != nil {
+		return user, nil
+	}
+
+	user.Name = input.Name
+	user.Email = input.Email
+	if user.Id == 0 {
+		return user, errors.New("not the user")
+	}
+
+	updateUser, err := s.userRepository.UpdateUser(user)
+	if err != nil {
+		return updateUser, nil
+	}
+
+	return user, nil
 }
